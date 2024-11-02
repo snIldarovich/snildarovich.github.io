f65e4b89724380457214a52279e66ad963df7962
shakirov <shakirov.n@cosmos-web.ru>
2024-11-02 09:46:48 +0500

    обновление

--- new-test/src/js/_vendor.js
+++
@@ -1,4 +0,0 @@
-import './vendor/focus-visible.js';
-
-// скрипты для select choices.js
-import './../../node_modules/choices.js/public/assets/scripts/choices.min.js';
--- new-test/src/js/components/smooth-scroll.js
+++
@@ -1,2 +0,0 @@
-import SmoothScroll from 'smooth-scroll';
-const scroll = new SmoothScroll('a[href*="#"]');
